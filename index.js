403c8d70cfa5c5a579a59b8cb005b5c9d64f24f4
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-07-23 15:48:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// 1-misol
+//  Berilgan 10 dan 100 gacha bo'lgan sonlar orasidagi barcha toq sonlarning yig'indisini hisoblang.
+// let res = 0;
+// for(let i = 10; i<=100; i++){
+//     if (i % 2 == 1) {
+//         res = res +i;
+//     }
+// }console.log(res);
+
+
+// 2-misol
+// Berilgan ixtiyoriy sonning faktorialini hisoblang.
+// let n = +prompt('n sonini kiritin');
+// let res = 1 ;
+// for (let i = 1; i<=n; i++){
+//     res *= i;
+// }console.log(res);
+
+// 3-misol
+// Berilgan 1 dan 50 gacha bo'lgan murakkab sonlarni aniqlang va ularni konsolga chiqaring.
+// let  num = 50;
+// let a = 0;
+// let arr = [];
+// for ( let i = 1; i<=num;i++){
+//     count = 0
+//     for (let k = 1; k<=i; k++){
+//         if (i %k == 0) {
+//             count++
+//         }
+//         if (count > 2) {
+//             arr.push(i);
+//             break
+//         }
+//     }
+// }console.log(arr);
+
+// 4-misol
+// 1 dan 20 gacha bo'lgan sonlarning kvadratlarini hisoblang va ularning yig'indisini aniqlang.
+// let a = 20;
+// let res = 1;
+// let sum  = 0;
+// for(let i = 1; i<=a; i++){
+//     res = i * i;
+//     sum = sum + res;
+//     console.log(res);
+// }
+// console.log(sum);
+
+// 5-misol
+// Foydalanuvchi tomonidan kiritilgan 5 ta ixtiyoriy sonlar ichida eng kattasi va eng kichigini toping.
+let num = [4, 5, 7, 9, 4];
+let numMax = math.max(...num)
+let numMax = math.min(...num)
+// 6-misol
